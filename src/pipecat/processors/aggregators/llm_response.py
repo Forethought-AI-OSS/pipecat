fa417d44798ebeeeb6c8d5324eb7cac24025659e
Weitian Xing <xingweitian@gmail.com>
2025-08-14 01:18:46 -0400

    Fix 'NoneType' object has no attribute 'cancel_on_interruption'.

--- src/pipecat/processors/aggregators/llm_response.py
+++ src/pipecat/processors/aggregators/llm_response.py
@@ -947,7 +947,7 @@ class LLMAssistantContextAggregator(LLMContextResponseAggregator):
         if frame.tool_call_id not in self._function_calls_in_progress:
             return
 
-        if self._function_calls_in_progress[frame.tool_call_id].cancel_on_interruption:
+        if self._function_calls_in_progress[frame.tool_call_id] and self._function_calls_in_progress[frame.tool_call_id].cancel_on_interruption:
             await self.handle_function_call_cancel(frame)
             del self._function_calls_in_progress[frame.tool_call_id]
 
